ab2fa42cb99c465a88d86e2254f0a199ab5dc8c7
wakimiko01000010 <link04024946@ezweb.ne.jp>
2019-02-19 03:20:24 +0900

    １次元imos法

---
+++ One-Dimensional-Imos.cpp
@@ -0,0 +1,17 @@
+//#include <vector>が必要
+
+struct Imos {
+    vector<int> num;
+
+    Imos(int N) : num(N) {}
+
+    void prepara(int r, int l, int n) { //足したい範囲の最初にnを、最後の次の要素に-nを足す
+        num[r] = n, num[l+1] = -n;
+    }
+
+    void cumulative() { //preparaでの情報をもとに累積和を求める
+        for (int i = 0; i < num.size(); i++) {
+            num[i] += num[i-1];
+        }
+    }
+};
