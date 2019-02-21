ec7ec1b4a22c931b4b6a253e3f8b3c20447ee3a2
wakimiko01000010 <link04024946@ezweb.ne.jp>
2019-02-21 09:42:52 +0900

    file update

--- Union-Find.cpp
+++ Union-Find.cpp
@@ -1,26 +1,31 @@
-int par[MAX_N]; //親の番号 par[i] = iならば根
+//#include <vector>が必要。
 
-void init(int n) { //初めは全部の頂点が根になる
-    for (int i = 0; i < n; i++) par[i] = i;
-}
+struct UnionFind {
+    vector<int> par;
 
-int root(int x) { //木の根を求める
-    if (par[x] == x) { //根の判定
-        return x;
+    UnionFind(int N) : par(N) {
+        //初めは全部の頂点が根になる
+        for (int i = 0; i < n; i++) par[i] = i;
     }
-    else {
-        return (par[x] = root(par[x])); //経路圧縮
-    }:
-}
 
-bool same(int x, int y) { //xとyが同じ集合か判定
-    return root(x) == root(y);
-}
+    int root(int x) { //木の根を求める
+        if (par[x] == x) { //根の判定
+            return x;
+        }
+        else {
+            return (par[x] = root(par[x])); //経路圧縮
+        }
+    }
 
-void unite(int x, int y) { //xとyの属する集合を併合する
-    x = root(x);
-    y = root(y);
-    if (x == y) return;
+    bool same(int x, int y) { //xとyが同じ集合か判定
+        return root(x) == root(y);
+    }
 
-    par[x] = y;
-}
+    void unite(int x, int y) { //xとyの属する集合を併合する
+        x = root(x);
+        y = root(y);
+        if (x == y) return; //根が同じ時はそのまま
+
+        par[x] = y; //xの根をyの根に付ける
+    }
+};
