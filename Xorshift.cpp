d6d8bcfbadbedac47f611fa1acd53b908103ff99
wakimiko01000010 <link04024946@ezweb.ne.jp>
2019-02-22 01:32:47 +0900

    add new file

---
+++ Xorshift.cpp
@@ -0,0 +1,11 @@
+//周期(2^128)-1の乱数
+
+unsigned int xorshift() {
+    static unsigned int tx = 123456789, ty = 362436069, tz = 521288629, tw = 88675123;
+    unsigned int tt = (tx^(tx<<11));
+    tx = ty;
+    ty = tz;
+    tz = tw;
+
+    return (tw=(tw^(tw>>19))^(tt^(tt>>8)));
+}
