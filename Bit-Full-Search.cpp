332d1f96c96b02898aa0d8fc60e51d37ad9eb66d
wakimiko01000010 <link04024946@ezweb.ne.jp>
2019-02-21 22:37:46 +0900

    add new file

---
+++ Bit-Full-Search.cpp
@@ -0,0 +1,16 @@
+int n = 5; //0 ~ n-1の部分集合の全探索
+
+for (int bit = 0; bit < (1<<n); bit++) {
+    vector<int> s;
+
+    for (int i = 0; i < n; i++) {
+        if (bit & (1<<i)) {
+            s.push_back(i);
+        }
+    }
+
+    for (int i = 0; i < (int)s.size(); i++) { //bitが表す集合を出力
+        cout << s[i] << " ";
+    }
+    cout << "\n";
+}
