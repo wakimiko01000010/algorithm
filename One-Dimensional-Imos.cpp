12efeb9cf784d4af672c0313f8874fc90994c9ce
wakimiko01000010 <link04024946@ezweb.ne.jp>
2019-03-02 00:17:56 +0900

    file update

--- One-Dimensional-Imos.cpp
+++ One-Dimensional-Imos.cpp
@@ -6,11 +6,11 @@ struct Imos {
     Imos(int N) : num(N) {}
 
     void prepara(int r, int l, int n) { //足したい範囲の最初にnを、最後の次の要素に-nを足す
-        num[r] = n, num[l+1] = -n;
+        num[r] += n, num[l+1] += -n;
     }
 
     void cumulative() { //preparaでの情報をもとに累積和を求める
-        for (int i = 0; i < num.size(); i++) {
+        for (int i = 1; i < num.size(); i++) {
             num[i] += num[i-1];
         }
     }
