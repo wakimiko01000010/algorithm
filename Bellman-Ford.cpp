9345bf0531fb7afd4b782e9399cfd15ec12fa618
wakimiko01000010 <link04024946@ezweb.ne.jp>
2019-03-03 23:48:31 +0900

    file update

--- Bellman-Ford.cpp
+++ Bellman-Ford.cpp
@@ -1,5 +1,5 @@
 int V; //頂点の数
-struct edge{int from, to, cost}; //それぞれ出発点、行き先、移動コスト
+struct edge{int from, to, cost;}; //それぞれ出発点、行き先、移動コスト
 vector<edge> edges;
 int d[MAX_V]; //d[i]は始点からiに行くのにかかる最短コスト
 
@@ -14,8 +14,8 @@ void bellmanford()
     for (int i = 0; i < V; i++) {
         for (int j = 0; j < (int)edges.size(); j++) {
             edge e = edges[j];
-            if (d[e.to] > d[e.from] + d[e.cost]) {
-                d[e.to] = d[e.from] + d[e.cost];
+            if (d[e.to] > d[e.from] + e.cost) {
+                d[e.to] = d[e.from] + e.cost;
                 if (i == V-1) {
                     cout << "negative loop" << endl;
                     return;
