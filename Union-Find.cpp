fb00c284be22db41c68cd4a615c89b343e2b3f99
wakimiko01000010 <link04024946@ezweb.ne.jp>
2019-05-20 13:16:20 +0900

    方式を変更

--- Union-Find.cpp
+++ Union-Find.cpp
@@ -3,18 +3,12 @@
 struct UnionFind {
     vector<int> par;
 
-    UnionFind(int N) : par(N) {
-        //初めは全部の頂点が根になる
-        for (int i = 0; i < N; i++) par[i] = i;
-    }
+    UnionFind(int N) : par(N, -1) {}
+    void init(int N) { par.assign(N, -1); }
 
     int root(int x) { //木の根を求める
-        if (par[x] == x) { //根の判定
-            return x;
-        }
-        else {
-            return (par[x] = root(par[x])); //経路圧縮
-        }
+        if (par[x] < 0) return x; //根の判定
+        else return (par[x] = root(par[x])); //経路圧縮
     }
 
     bool same(int x, int y) { //xとyが同じ集合か判定
@@ -22,12 +16,14 @@ struct UnionFind {
     }
 
     bool merge(int x, int y) { //xとyの属する集合を併合する
-        x = root(x);
-        y = root(y);
+        x = root(x); y = root(y);
         if (x == y) return false; //根が同じ時はそのまま
-        if (par[x] > par[y]) swap(x, y);
+        if (par[x] > par[y]) swap(x, y); //要素が少ない方をくっつける
         par[x] += par[y];
         par[y] = x;
         return true;
+    }
+    int size(int x) {
+        return -par[root(x)];
     }
 };
