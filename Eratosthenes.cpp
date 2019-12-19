15aad523622ef3d331eb408978d6b039227cd92e
wakimiko01000010 <43177242+wakimiko01000010@users.noreply.github.com>
2019-12-19 10:34:27 +0900

    Update Eratosthenes.cpp

--- Eratosthenes.cpp
+++ Eratosthenes.cpp
@@ -7,7 +7,7 @@ void eratosthenes(int n) { //n以下の素数判定
 
     for (int i = 2; i*i <= n; i++) {
         if (num[i]) {
-            for (int j = 2*i; j <= i; j += i) {
+            for (int j = 2*i; j <= n; j += i) {
                 num[j] = false;
             }
         }
